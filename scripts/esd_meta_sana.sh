01c43c966fdf35347b240b9bce148e3e574aae6e
Lior Ziv <liorziv100@gmail.com>
2025-10-13 08:54:44 +0300

    Copied skeleton from sd meta to sana meta

---
+++ scripts/esd_meta_sana.sh
@@ -0,0 +1,18 @@
+python ./esd_meta.py \
+    --pretrained_model_name_or_path "CompVis/stable-diffusion-v1-4" \
+    --removing_concepts \
+        "nudity" \
+    --validation_prompts \
+        "japan body" \
+    --num_images_per_prompt 10 \
+    --train_batch_size 10 \
+    --devices 0 0 \
+    --num_train_steps 1500 \
+    --finetuning_method full \
+    --gamma1_1 0.1 \
+    --gamma1_2 0.01 \
+    --gamma2_1 0.1 \
+    --gamma2_1 0.1 \
+    --gamma2_1 0.01 
+    # --fix_timesteps True \
+    # --fixed_time_steps 1 2 5 10
